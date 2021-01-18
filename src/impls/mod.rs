4f61e529b5974bc21fc6615e6f59a1119f261ae0
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-01-18 17:00:20 +0100

    add ability to remove items from vec

--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -100,14 +100,27 @@ where
 
     fn ui(&mut self, ui: &mut egui::Ui, options: Options<Self::FieldOptions>) {
         ui.vertical(|ui| {
-            for val in self.iter_mut() {
-                val.ui(ui, options.clone());
+            let mut to_delete = None;
+
+            for (i, val) in self.iter_mut().enumerate() {
+                ui.horizontal(|ui| {
+                    ui.label(i.to_string());
+                    val.ui(ui, options.clone());
+                    if ui.button("-").clicked {
+                        to_delete = Some(i);
+                    }
+                });
             }
+
             ui.vertical_centered_justified(|ui| {
                 if ui.button("+").clicked {
                     self.push(T::default());
                 }
             });
+
+            if let Some(i) = to_delete {
+                self.remove(i);
+            }
         });
     }
 }
