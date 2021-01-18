86e5a1cae4e329f5e0cc1661811778de994727a3
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-01-18 14:32:16 +0100

    add speed to NumberAttributes

--- src/impls/mod.rs
+++ src/impls/mod.rs
@@ -12,10 +12,15 @@ use egui::widgets;
 pub struct NumberAttributes {
     pub min: f32,
     pub max: f32,
+    pub speed: f32,
 }
 impl Default for NumberAttributes {
     fn default() -> Self {
-        NumberAttributes { min: 0.0, max: 1.0 }
+        NumberAttributes {
+            min: 0.0,
+            max: 1.0,
+            speed: 0.1,
+        }
     }
 }
 
@@ -26,7 +31,8 @@ macro_rules! impl_for_num {
 
             fn ui(&mut self, ui: &mut egui::Ui, options: Options<Self::FieldOptions>) {
                 let widget = widgets::DragValue::$ty(self)
-                    .range(options.custom.min..=options.custom.max);
+                    .range(options.custom.min..=options.custom.max)
+                    .speed(options.custom.speed);
                 ui.add(widget);
             }
         }
