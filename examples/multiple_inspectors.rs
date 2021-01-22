980de51e181fe486ac74312474c33a34e0a77293
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-01-22 14:47:51 +0100

    allow multiple inspectors

---
+++ examples/multiple_inspectors.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::{Inspectable, InspectorPlugin};
+
+#[derive(Inspectable, Default)]
+struct UiData {
+    font_size: f32,
+    color: Color,
+}
+#[derive(Inspectable, Default)]
+struct TransformData {
+    transform: Transform,
+    font_size: f32,
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(InspectorPlugin::<UiData>::new())
+        .add_plugin(InspectorPlugin::<TransformData>::new())
+        .run();
+}
--- src/plugin.rs
+++ src/plugin.rs
@@ -1,4 +1,4 @@
-use std::marker::PhantomData;
+use std::{any::TypeId, marker::PhantomData};
 
 use bevy::prelude::*;
 use bevy_egui::{egui, EguiContext, EguiPlugin};
@@ -17,24 +17,73 @@ impl<T> InspectorPlugin<T> {
     }
 }
 
+#[derive(Default, Debug)]
+struct InspectorWindows(bevy::utils::HashMap<TypeId, String>);
+impl InspectorWindows {
+    fn contains_id(&self, type_id: TypeId) -> bool {
+        self.0.iter().any(|(&id, _)| id == type_id)
+    }
+    fn contains_name(&self, name: &str) -> bool {
+        self.0.iter().any(|(_, n)| n == name)
+    }
+}
+
 impl<T> Plugin for InspectorPlugin<T>
 where
     T: Inspectable + Default + Send + Sync + 'static,
 {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_plugin(EguiPlugin)
-            .init_resource::<T>()
-            .add_system(ui::<T>.system());
+        // init inspector ui and data resource
+        app.init_resource::<T>().add_system(ui::<T>.system());
+
+        // init egui
+        if !app.resources().contains::<EguiContext>() {
+            app.add_plugin(EguiPlugin);
+        }
+
+        // add entry to `InspectorWindows`
+        let mut inspector_windows = app
+            .resources_mut()
+            .get_or_insert_with(InspectorWindows::default);
+
+        let type_id = TypeId::of::<T>();
+        let full_type_name = std::any::type_name::<T>();
+
+        if inspector_windows.contains_id(type_id) {
+            panic!(
+                "InspectorPlugin for {} is already registered",
+                full_type_name,
+            );
+        }
+
+        let type_name = full_type_name.rsplit("::").next().unwrap_or("Inspector");
+        if inspector_windows.contains_name(type_name) {
+            if inspector_windows.contains_name(full_type_name) {
+                panic!("two types with different type_id but same type_name");
+            } else {
+                inspector_windows.0.insert(type_id, full_type_name.into());
+            }
+        } else {
+            inspector_windows.0.insert(type_id, type_name.into());
+        }
     }
 }
 
-fn ui<T>(mut egui_context: ResMut<EguiContext>, mut data: ResMut<T>)
-where
+fn ui<T>(
+    mut egui_context: ResMut<EguiContext>,
+    inspector_windows: Res<InspectorWindows>,
+    mut data: ResMut<T>,
+) where
     T: Inspectable + Send + Sync + 'static,
 {
     let ctx = &mut egui_context.ctx;
 
-    egui::Window::new("Inspector")
+    let type_name = inspector_windows
+        .0
+        .get(&TypeId::of::<T>())
+        .expect("inspector window not properly initialized");
+
+    egui::Window::new(type_name)
         .resizable(false)
         .show(ctx, |ui| {
             data.ui(ui, T::Attributes::default());
