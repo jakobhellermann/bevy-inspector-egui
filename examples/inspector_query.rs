751553896aa2af697e882f7e82b9029e1c5fa3a1
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-01-31 20:19:17 +0100

    add inspector query example

---
+++ examples/inspector_query.rs
@@ -0,0 +1,43 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::{widgets::InspectorQuery, Inspectable, InspectorPlugin};
+
+#[derive(Inspectable, Default)]
+struct Data {
+    query: InspectorQuery<With<Transform>>,
+}
+
+fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(InspectorPlugin::<Data>::new())
+        .add_startup_system(setup.system())
+        .run();
+}
+
+fn setup(
+    commands: &mut Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..Default::default()
+        })
+        .spawn(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_translation(Vec3::new(0.0, 0.5, 0.0)),
+            ..Default::default()
+        })
+        .spawn(LightBundle {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        })
+        .spawn(PerspectiveCameraBundle {
+            transform: Transform::from_translation(Vec3::new(-2.0, 2.5, 5.0))
+                .looking_at(Vec3::default(), Vec3::unit_y()),
+            ..Default::default()
+        });
+}
