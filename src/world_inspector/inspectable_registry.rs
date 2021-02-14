757c09a47169ec4b1ab36b5efcb160999c4d4492
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-02-14 15:59:37 +0100

    InspectableRegistry::{register_raw, with}

--- src/world_inspector/inspectable_registry.rs
+++ src/world_inspector/inspectable_registry.rs
@@ -8,7 +8,7 @@ use std::any::TypeId;
 type InspectCallback = Box<dyn Fn(*mut u8, &mut egui::Ui, &Context) + Send + Sync>;
 
 /// The `InspectableRegistry` can be used to tell the [`WorldInspectorPlugin`](crate::WorldInspectorPlugin)
-/// that a type implements [`Inspectable`](crate::Inspectable).
+/// how to display a type.
 pub struct InspectableRegistry {
     impls: HashMap<TypeId, InspectCallback>,
 }
@@ -44,12 +44,37 @@ impl Default for InspectableRegistry {
 impl InspectableRegistry {
     /// Register type `T` so that it can be displayed by the [`WorldInspectorPlugin`](crate::WorldInspectorPlugin).
     pub fn register<T: Inspectable + 'static>(&mut self) {
-        let type_id = TypeId::of::<T>();
-        let callback = Box::new(|ptr: *mut u8, ui: &mut egui::Ui, context: &Context| {
-            let value: &mut T = unsafe { &mut *(ptr as *mut T) };
+        self.register_raw::<T, _>(|value, ui, context| {
             value.ui(ui, <T as Inspectable>::Attributes::default(), context)
+        });
+    }
+
+    /// Registers a type that doesn't need to implement [`Inspectable`](crate::Inspectable)
+    pub fn register_raw<T: 'static, F>(&mut self, f: F)
+    where
+        F: Fn(&mut T, &mut egui::Ui, &Context) + Send + Sync + 'static,
+    {
+        let type_id = TypeId::of::<T>();
+        let callback = Box::new(move |ptr: *mut u8, ui: &mut egui::Ui, context: &Context| {
+            let value: &mut T = unsafe { &mut *(ptr as *mut T) };
+            f(value, ui, context);
         }) as InspectCallback;
         self.impls.insert(type_id, callback);
+    }
+
+    /// Variant of [`InspectableRegistry::register`] which returns self by-value.
+    /// Allows
+    /// ```rust,no_run
+    /// # use bevy::prelude::*;
+    /// # use bevy_inspector_egui::InspectableRegistry;
+    /// # #[derive(bevy_inspector_egui::Inspectable)] struct MyType;
+    /// App::build()
+    ///   .insert_resource(InspectableRegistry::default().with::<MyType>())
+    ///   .run();
+    /// ```
+    pub fn with<T: Inspectable + 'static>(mut self) -> Self {
+        self.register::<T>();
+        self
     }
 
     pub(crate) fn try_execute(
