cced64e82f20f5b2c70febb402e6a93246305489
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-05-16 20:39:46 +0200

    impl for DirectionalLight

--- src/world_inspector/inspectable_registry.rs
+++ src/world_inspector/inspectable_registry.rs
@@ -106,6 +106,7 @@ impl Default for InspectableRegistry {
         this.register::<TextureAtlasSprite>();
         this.register::<TextureAtlas>();
         this.register::<PointLight>();
+        this.register::<DirectionalLight>();
         this.register::<StandardMaterial>();
         this.register::<ColorMaterial>();
         this.register::<PrimitiveTopology>();
