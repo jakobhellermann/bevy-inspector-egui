56b10bd3efc06f035325430084b46d7d6ae73719
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-07-24 20:13:24 +0200

    remove blanket impl for numbers to simplify rustc's errors

--- src/impls/number.rs
+++ src/impls/number.rs
@@ -64,78 +64,84 @@ impl NumberAttributes<f32> {
     }
 }
 
-pub trait Num: emath::Numeric {
-    fn default_speed() -> Option<f32> {
-        None
-    }
+macro_rules! impl_num {
+    ($ty:ty $(| $default_speed:literal)?) => {
+        impl Inspectable for $ty {
+            type Attributes = NumberAttributes<$ty>;
+
+            fn ui(&mut self, ui: &mut egui::Ui, options: Self::Attributes, _: &Context) -> bool {
+                num_ui(self, options, ui, None)
+            }
+        }
+    };
 }
 
-impl Num for f32 {
-    fn default_speed() -> Option<f32> {
-        Some(0.1)
-    }
-}
-impl Num for f64 {
-    fn default_speed() -> Option<f32> {
-        Some(0.1)
-    }
-}
-impl Num for i8 {}
-impl Num for u8 {}
-impl Num for i16 {}
-impl Num for u16 {}
-impl Num for i32 {}
-impl Num for u32 {}
-impl Num for i64 {}
-impl Num for u64 {}
-impl Num for isize {}
-impl Num for usize {}
+impl_num!(i8);
+impl_num!(i16);
+impl_num!(i32);
+impl_num!(i64);
+impl_num!(isize);
+impl_num!(u8);
+impl_num!(u16);
+impl_num!(u32);
+impl_num!(u64);
+impl_num!(usize);
 
-impl<T: Num> Inspectable for T {
-    type Attributes = NumberAttributes<T>;
+impl Inspectable for f32 {
+    type Attributes = NumberAttributes<f32>;
 
     fn ui(&mut self, ui: &mut egui::Ui, options: Self::Attributes, _: &Context) -> bool {
-        let mut widget = widgets::DragValue::new(self);
-
-        if !options.prefix.is_empty() {
-            widget = widget.prefix(options.prefix);
-        }
-        if !options.suffix.is_empty() {
-            widget = widget.suffix(options.suffix);
-        }
-
-        match (options.min, options.max) {
-            (Some(min), Some(max)) => widget = widget.clamp_range(min.to_f64()..=max.to_f64()),
-            (Some(min), None) => widget = widget.clamp_range(min.to_f64()..=f64::MAX),
-            (None, Some(max)) => widget = widget.clamp_range(f64::MIN..=max.to_f64()),
-            (None, None) => {}
-        }
-
-        if options.speed != 0.0 {
-            widget = widget.speed(options.speed);
-        } else if let Some(default_speed) = T::default_speed() {
-            widget = widget.speed(default_speed);
-        }
-
-        let mut changed = ui.add(widget).changed();
-
-        if let Some(min) = options.min {
-            let as_f64 = self.to_f64();
-            let min = min.to_f64();
-            if as_f64 < min {
-                *self = T::from_f64(min);
-                changed = true;
-            }
-        }
-        if let Some(max) = options.max {
-            let as_f64 = self.to_f64();
-            let max = max.to_f64();
-            if as_f64 > max {
-                *self = T::from_f64(max);
-                changed = true;
-            }
-        }
-
-        changed
+        num_ui(self, options, ui, Some(0.1))
     }
+}
+impl Inspectable for f64 {
+    type Attributes = NumberAttributes<f64>;
+
+    fn ui(&mut self, ui: &mut egui::Ui, options: Self::Attributes, _: &Context) -> bool {
+        num_ui(self, options, ui, Some(0.1))
+    }
+}
+
+fn num_ui<T: emath::Numeric>(
+    value: &mut T,
+    options: NumberAttributes<T>,
+    ui: &mut egui::Ui,
+    default_speed: Option<f32>,
+) -> bool {
+    let mut widget = widgets::DragValue::new(value);
+    if !options.prefix.is_empty() {
+        widget = widget.prefix(options.prefix);
+    }
+    if !options.suffix.is_empty() {
+        widget = widget.suffix(options.suffix);
+    }
+    match (options.min, options.max) {
+        (Some(min), Some(max)) => widget = widget.clamp_range(min.to_f64()..=max.to_f64()),
+        (Some(min), None) => widget = widget.clamp_range(min.to_f64()..=f64::MAX),
+        (None, Some(max)) => widget = widget.clamp_range(f64::MIN..=max.to_f64()),
+        (None, None) => {}
+    }
+    if options.speed != 0.0 {
+        widget = widget.speed(options.speed);
+    } else if let Some(default_speed) = default_speed {
+        widget = widget.speed(default_speed);
+    }
+    let mut changed = ui.add(widget).changed();
+    if let Some(min) = options.min {
+        let as_f64 = value.to_f64();
+        let min = min.to_f64();
+        if as_f64 < min {
+            *value = T::from_f64(min);
+            changed = true;
+        }
+    }
+    if let Some(max) = options.max {
+        let as_f64 = value.to_f64();
+        let max = max.to_f64();
+        if as_f64 > max {
+            *value = T::from_f64(max);
+            changed = true;
+        }
+    }
+    changed
 }
