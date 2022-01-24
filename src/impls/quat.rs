841ad89463312f965d7b101961c069bb3343c28c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-01-24 13:20:04 +0100

    refactor quat inspecting, fixes external changes

--- src/impls/quat.rs
+++ src/impls/quat.rs
@@ -24,12 +24,117 @@ impl Default for QuatAttributes {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct Euler(Vec3);
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct YawPitchRoll((f32, f32, f32));
-#[derive(Clone)]
+#[derive(Clone, Copy)]
 struct AxisAngle((Vec3, f32));
+
+trait RotationEdit {
+    fn from_quat(quat: Quat) -> Self;
+    fn to_quat(self) -> Quat;
+
+    fn ui(&mut self, ui: &mut egui::Ui, context: &mut Context) -> bool;
+}
+
+impl RotationEdit for Euler {
+    fn from_quat(quat: Quat) -> Self {
+        Euler(quat.to_euler(EulerRot::XYZ).into())
+    }
+
+    fn to_quat(self) -> Quat {
+        Quat::from_euler(EulerRot::XYZ, self.0.x, self.0.y, self.0.z)
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, context: &mut Context) -> bool {
+        self.0.ui(ui, Default::default(), context)
+    }
+}
+
+impl RotationEdit for YawPitchRoll {
+    fn from_quat(quat: Quat) -> Self {
+        YawPitchRoll(quat.to_euler(EulerRot::YXZ))
+    }
+
+    fn to_quat(self) -> Quat {
+        let (y, p, r) = self.0;
+        Quat::from_euler(EulerRot::YXZ, y, p, r)
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, _: &mut Context) -> bool {
+        let (yaw, pitch, roll) = &mut self.0;
+
+        let mut changed = false;
+        ui.vertical(|ui| {
+            egui::Grid::new("ypr grid").show(ui, |ui| {
+                ui.label("Yaw");
+                changed |= ui.drag_angle(yaw).changed();
+                ui.end_row();
+                ui.label("Pitch").changed();
+                changed |= ui.drag_angle(pitch).changed();
+                ui.end_row();
+                ui.label("Roll");
+                changed |= ui.drag_angle(roll).changed();
+                ui.end_row();
+            });
+        });
+        changed
+    }
+}
+
+impl RotationEdit for AxisAngle {
+    fn from_quat(quat: Quat) -> Self {
+        AxisAngle(quat.to_axis_angle())
+    }
+
+    fn to_quat(self) -> Quat {
+        let (axis, angle) = self.0;
+        Quat::from_axis_angle(axis.normalize(), angle)
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, context: &mut Context) -> bool {
+        let (axis, angle) = &mut self.0;
+
+        let mut changed = false;
+        ui.vertical(|ui| {
+            egui::Grid::new("axis-angle quat").show(ui, |ui| {
+                ui.label("Axis");
+                changed |= axis.ui(ui, Default::default(), context);
+                ui.end_row();
+                ui.label("Angle");
+                changed |= ui.drag_angle(angle).changed();
+                ui.end_row();
+            });
+        });
+        changed
+    }
+}
+
+fn quat_ui<T: Send + Sync + 'static + Copy + RotationEdit>(
+    val: &mut Quat,
+    ui: &mut egui::Ui,
+    context: &mut Context,
+) -> bool {
+    let mut intermediate = *ui
+        .memory()
+        .data
+        .get_temp_mut_or_insert_with(context.id(), || T::from_quat(*val));
+
+    let externally_changed = !intermediate.to_quat().abs_diff_eq(*val, std::f32::EPSILON);
+    if externally_changed {
+        intermediate = T::from_quat(*val);
+    }
+
+    let changed = intermediate.ui(ui, context);
+
+    if changed || externally_changed {
+        *val = intermediate.to_quat();
+        ui.memory().data.insert_temp(context.id(), intermediate);
+    }
+
+    changed
+}
 
 impl Inspectable for Quat {
     type Attributes = QuatAttributes;
@@ -46,88 +151,9 @@ impl Inspectable for Quat {
                 }
                 changed
             }
-            QuatDisplay::Euler => {
-                let mut euler_angles = ui
-                    .memory()
-                    .data
-                    .get_temp_mut_or_insert_with(context.id(), || {
-                        Euler(Vec3::from(self.to_euler(EulerRot::XYZ)))
-                    })
-                    .0;
-
-                let changed = euler_angles.ui(ui, Default::default(), context);
-                if changed {
-                    *self = Quat::from_euler(
-                        EulerRot::XYZ,
-                        euler_angles.x,
-                        euler_angles.y,
-                        euler_angles.z,
-                    );
-                    ui.memory()
-                        .data
-                        .insert_temp(context.id(), Euler(euler_angles));
-                }
-                changed
-            }
-            QuatDisplay::YawPitchRoll => {
-                let (mut yaw, mut pitch, mut roll) = ui
-                    .memory()
-                    .data
-                    .get_temp_mut_or_insert_with(context.id(), || {
-                        YawPitchRoll(self.to_euler(EulerRot::YXZ))
-                    })
-                    .0;
-
-                let mut changed = false;
-                ui.vertical(|ui| {
-                    egui::Grid::new("ypr grid").show(ui, |ui| {
-                        ui.label("Yaw");
-                        changed |= ui.drag_angle(&mut yaw).changed();
-                        ui.end_row();
-                        ui.label("Pitch").changed();
-                        changed |= ui.drag_angle(&mut pitch).changed();
-                        ui.end_row();
-                        ui.label("Roll");
-                        changed |= ui.drag_angle(&mut roll).changed();
-                        ui.end_row();
-                    });
-                });
-
-                if changed {
-                    *self = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
-                    ui.memory()
-                        .data
-                        .insert_temp(context.id(), YawPitchRoll((yaw, pitch, roll)));
-                }
-
-                changed
-            }
-            QuatDisplay::AxisAngle => {
-                let (mut axis, mut angle) = ui
-                    .memory()
-                    .data
-                    .get_temp_mut_or_insert_with(context.id(), || AxisAngle(self.to_axis_angle()))
-                    .0;
-
-                let mut changed = false;
-                ui.vertical(|ui| {
-                    egui::Grid::new("axis-angle quat").show(ui, |ui| {
-                        ui.label("Axis");
-                        changed |= axis.ui(ui, Default::default(), context);
-                        ui.end_row();
-                        ui.label("Angle");
-                        changed |= ui.drag_angle(&mut angle).changed();
-                        ui.end_row();
-                    });
-                });
-                if changed {
-                    *self = Quat::from_axis_angle(axis.normalize(), angle);
-                    ui.memory()
-                        .data
-                        .insert_temp(context.id(), AxisAngle((axis, angle)));
-                }
-                changed
-            }
+            QuatDisplay::Euler => quat_ui::<Euler>(self, ui, context),
+            QuatDisplay::YawPitchRoll => quat_ui::<YawPitchRoll>(self, ui, context),
+            QuatDisplay::AxisAngle => quat_ui::<AxisAngle>(self, ui, context),
         }
     }
 }
