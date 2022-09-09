d0c834f3b6256b5202bc72cb72d8610cd681ec68
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-09-09 19:42:38 +0200

    add texture example

---
+++ crates/bevy-inspector-egui/examples/texture.rs
@@ -0,0 +1,96 @@
+use std::f32::consts::PI;
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(bevy_egui::EguiPlugin)
+        .add_system(ui_example.exclusive_system())
+        .add_startup_system(setup)
+        .run();
+}
+
+fn ui_example(world: &mut World) {
+    world.resource_scope::<bevy_egui::EguiContext, _>(|world, mut egui_context| {
+        egui::Window::new("Hello").show(egui_context.ctx_mut(), |ui| {
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                bevy_inspector_egui::bevy_ecs_inspector::ui_for_world(world, ui);
+            });
+        });
+    });
+}
+
+/// sets up a scene with textured entities
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // load a texture and retrieve its aspect ratio
+    let texture_handle = asset_server.load("branding/bevy_logo_dark_big.png");
+    let aspect = 0.25;
+
+    // create a new quad mesh. this is what we will apply the texture to
+    let quad_width = 8.0;
+    let quad_handle = meshes.add(Mesh::from(shape::Quad::new(Vec2::new(
+        quad_width,
+        quad_width * aspect,
+    ))));
+
+    // this material renders the texture normally
+    let material_handle = materials.add(StandardMaterial {
+        base_color_texture: Some(texture_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+
+    // this material modulates the texture to make it red (and slightly transparent)
+    let red_material_handle = materials.add(StandardMaterial {
+        base_color: Color::rgba(1.0, 0.0, 0.0, 0.5),
+        base_color_texture: Some(texture_handle.clone()),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+
+    // and lets make this one blue! (and also slightly transparent)
+    let blue_material_handle = materials.add(StandardMaterial {
+        base_color: Color::rgba(0.0, 0.0, 1.0, 0.5),
+        base_color_texture: Some(texture_handle),
+        alpha_mode: AlphaMode::Blend,
+        unlit: true,
+        ..default()
+    });
+
+    // textured quad - normal
+    commands.spawn_bundle(PbrBundle {
+        mesh: quad_handle.clone(),
+        material: material_handle,
+        transform: Transform::from_xyz(0.0, 0.0, 1.5)
+            .with_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        ..default()
+    });
+    // textured quad - modulated
+    commands.spawn_bundle(PbrBundle {
+        mesh: quad_handle.clone(),
+        material: red_material_handle,
+        transform: Transform::from_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        ..default()
+    });
+    // textured quad - modulated
+    commands.spawn_bundle(PbrBundle {
+        mesh: quad_handle,
+        material: blue_material_handle,
+        transform: Transform::from_xyz(0.0, 0.0, -1.5)
+            .with_rotation(Quat::from_rotation_x(-PI / 5.0)),
+        ..default()
+    });
+    // camera
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(3.0, 5.0, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
