4cbabb80ca503ac25faa65ac6c0692b587637ab5
Jakob Hellermann <jakob.hellermann@protonmail.com>
2022-11-14 11:30:04 +0100

    reorganize examples

---
+++ crates/bevy-inspector-egui/examples/quick/resource_inspector.rs
@@ -0,0 +1,57 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::prelude::*;
+use bevy_inspector_egui::quick::ResourceInspectorPlugin;
+
+#[derive(Reflect, Resource, Default, InspectorOptions)]
+#[reflect(Resource, InspectorOptions)]
+struct Configuration {
+    name: String,
+    #[inspector(min = 0.0, max = 1.0)]
+    option: f32,
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(ResourceInspectorPlugin::<Configuration>::default())
+        .init_resource::<Configuration>()
+        .register_type::<Configuration>()
+        .add_startup_system(setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+    // cube
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+    // light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+    // camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
--- crates/bevy-inspector-egui/examples/quick/world_inspector.rs
+++ crates/bevy-inspector-egui/examples/quick/world_inspector.rs
@@ -1,36 +1,12 @@
 use bevy::prelude::*;
-use bevy_inspector_egui::prelude::*;
-use bevy_inspector_egui::quick::{
-    ResourceInspectorPlugin, StateInspectorPlugin, WorldInspectorPlugin,
-};
-
-#[derive(Reflect, Resource, Default, InspectorOptions)]
-#[reflect(Resource, InspectorOptions)]
-struct Configuration {
-    name: String,
-    #[inspector(min = 0.0, max = 1.0)]
-    option: f32,
-}
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(WorldInspectorPlugin)
-        .add_plugin(ResourceInspectorPlugin::<Configuration>::default())
-        .add_plugin(StateInspectorPlugin::<AppState>::default())
-        .add_state(AppState::A)
-        .init_resource::<Configuration>()
-        .register_type::<Configuration>()
-        .register_type::<AppState>()
         .add_startup_system(setup)
         .run();
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Reflect)]
-enum AppState {
-    A,
-    B,
-    C,
 }
 
 /// set up a simple 3D scene
