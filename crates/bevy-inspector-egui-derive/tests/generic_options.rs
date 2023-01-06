873a00586d2e4f1c91110ccc478da4c776204e15
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-01-06 15:42:56 +0100

    add test for PhantomData

--- crates/bevy-inspector-egui-derive/tests/generic_options.rs
+++ crates/bevy-inspector-egui-derive/tests/generic_options.rs
@@ -1,3 +1,5 @@
+use std::marker::PhantomData;
+
 use bevy_inspector_egui::{
     inspector_options::{std_options::NumberOptions, Target},
     InspectorOptions,
@@ -18,6 +20,27 @@ fn generic_without_options() {
 
     let options = options
         .get(Target::Field(1))
+        .unwrap()
+        .downcast_ref::<NumberOptions<f32>>()
+        .unwrap();
+    assert_eq!(options.min, Some(0.0));
+}
+
+#[test]
+fn phantom_data() {
+    #[derive(Reflect, InspectorOptions)]
+    struct Generic<T: 'static> {
+        #[reflect(ignore)]
+        _marker: PhantomData<fn() -> T>,
+
+        #[inspector(min = 0.0)]
+        other: f32,
+    }
+
+    let options = <InspectorOptions as FromType<Generic<f32>>>::from_type();
+
+    let options = options
+        .get(Target::Field(0))
         .unwrap()
         .downcast_ref::<NumberOptions<f32>>()
         .unwrap();
