0751fda799ecd5b90d135580c996b9eb65dc8704
Mahesh K Bhat <k.mahesh.bhat@gmail.com>
2023-07-10 23:46:36 +0530

    uncomment commented tests

--- crates/bevy-inspector-egui/examples/quick/world_inspector.rs
+++ crates/bevy-inspector-egui/examples/quick/world_inspector.rs
@@ -32,7 +32,7 @@ fn setup(
         ..default()
     });
     // light
-    /*commands.spawn(PointLightBundle {
+    commands.spawn(PointLightBundle {
         point_light: PointLight {
             intensity: 1500.0,
             shadows_enabled: true,
@@ -40,7 +40,7 @@ fn setup(
         },
         transform: Transform::from_xyz(4.0, 8.0, 4.0),
         ..default()
-    });*/
+    });
     // camera
     commands.spawn(Camera3dBundle {
         transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
