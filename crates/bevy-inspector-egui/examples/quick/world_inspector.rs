c05f19af0e4c5bf392c4851fe8d6fe8b8c9c5fc4
Azorlogh <bott.alix@gmail.com>
2023-12-13 11:45:59 +0100

    show entity identifier besides name

--- crates/bevy-inspector-egui/examples/quick/world_inspector.rs
+++ crates/bevy-inspector-egui/examples/quick/world_inspector.rs
@@ -25,12 +25,15 @@ fn setup(
         ..default()
     });
     // cube
-    commands.spawn(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
+    commands.spawn((
+        Name::new("My Cube"),
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..default()
+        },
+    ));
     // light
     commands.spawn(PointLightBundle {
         point_light: PointLight {
