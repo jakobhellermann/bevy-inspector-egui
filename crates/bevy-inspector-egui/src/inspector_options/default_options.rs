0da137233f17f15651a4e006bc28a51cf2fe5384
Thierry Berger <contact@thierryberger.com>
2024-11-07 09:46:20 +0100

    pr feedbacks

--- crates/bevy-inspector-egui/src/inspector_options/default_options.rs
+++ crates/bevy-inspector-egui/src/inspector_options/default_options.rs
@@ -12,6 +12,7 @@ fn insert_options_struct<T: 'static>(
     fields: &[(&'static str, &dyn TypeData)],
 ) {
     let Some(registration) = type_registry.get_mut(std::any::TypeId::of::<T>()) else {
+        bevy_log::warn!("Attempting to set default inspector options for {}, but it wasn't registered in the type registry.", std::any::type_name::<T>());
         return;
     };
     if registration.data::<ReflectInspectorOptions>().is_none() {
@@ -34,6 +35,7 @@ fn insert_options_enum<T: 'static>(
     fields: &[(&'static str, &'static str, &dyn TypeData)],
 ) {
     let Some(registration) = type_registry.get_mut(std::any::TypeId::of::<T>()) else {
+        bevy_log::warn!("Attempting to set default inspector options for {}, but it wasn't registered in the type registry.", std::any::type_name::<T>());
         return;
     };
     if registration.data::<ReflectInspectorOptions>().is_none() {
